05c1357166884046af28ede3391bb21dd51490cc
Loganbush97 <69697014+Loganbush97@users.noreply.github.com>
2020-09-17 14:41:30 -0500

    Add files via upload

---
+++ WeeklyChallenges/ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ChallengesWithTestsMark8
+{
+    public class ChallengesSet01
+    {
+        public bool AreTwoNumbersTheSame(int num1, int num2)
+        {
+            if(num1 == num2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public double Subtract(double minuend, double subtrahend)
+        {
+            double result = minuend - subtrahend;
+            return result; 
+            
+        }
+
+        public int Add(int number1, int number2)
+        {
+            int sum = number1 + number2;
+            return sum;
+        }
+
+        public int GetSmallestNumber(int number1, int number2)
+        {
+            int smallestNumber = 0;
+            int[] a = new int[2];
+            a[0] = number1;
+            a[1] = number2;
+            Array.Sort(a);
+            smallestNumber = a[0];
+            return smallestNumber;
+        }
+
+        public long Multiply(long factor1, long factor2)
+        {
+            return factor1 * factor2;
+        }
+
+        public string GetGreeting(string nameOfPerson)
+        {
+            var greeting = "Hello!";
+            if (nameOfPerson.Length > 0)
+            {
+                greeting = $"Hello, {nameOfPerson}!";
+            }
+
+            return greeting;
+        }
+
+        public string GetHey()
+        {
+            return "Hey";
+        }
+    }
+}
---
+++ WeeklyChallenges/ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChallengesWithTestsMark8
+{
+    public class ChallengesSet02
+    {
+        public bool CharacterIsALetter(char c)
+        {
+            return Char.IsLetter(c);
+        }
+
+        public bool CountOfElementsIsEven(string[] vals)
+        {
+            int length = vals.Length;
+            return length % 2 == 0;
+        }
+
+        public bool IsNumberEven(int number)
+        {
+            if (number % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IsNumberOdd(int num)
+        {
+            if (num % 2 != 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public double SumOfMinAndMax(IEnumerable<double> numbers)
+        {
+            if (!numbers?.Any() ?? false)
+            {
+                return 0;
+            }
+            var min = numbers.Min(i => i);
+            var max = numbers.Max(k => k);
+
+            return min + max;
+        }
+
+        public int GetLengthOfShortestString(string str1, string str2)
+        {
+           if (str1.Length > str2.Length)
+            {
+                return str2.Length;
+            }
+        }
+
+        public int Sum(int[] numbers)
+        {
+            var total = 0;
+
+            if (numbers == null)
+            {
+                return 0;
+            }
+
+            foreach (var item in numbers)
+            {
+                total += item;
+            }
+
+            return total;
+        }
+
+        public int SumEvens(int[] numbers)
+        {
+            int sum = 0;
+            if (numbers?.Any() != true)
+            {
+                return sum;
+            }
+            else
+            {
+                foreach (var num in numbers)
+                {
+                    if (num % 2 == 0)
+                    {
+                        sum += num;
+                    }
+
+                }
+            }
+            return sum;
+        }
+
+        public bool IsSumOdd(List<int> numbers)
+        {
+            if (numbers?.Any() != true)
+            {
+                return false;
+            }
+            else
+            {
+                var sum = numbers.Sum();
+                return sum % 2 != 0;
+            }
+
+        }
+
+        public long CountOfPositiveOddsBelowNumber(long number)
+        {
+            long oddSum = 0;
+            for (var i = 1; i < number; i += 2)
+            {
+                oddSum += 1;
+            }
+
+            return oddSum;
+        }
+    }
+}
+    }
+}
